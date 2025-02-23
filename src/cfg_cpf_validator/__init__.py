6cdfe389cd3bde4ee1b638e91194bf95261efbf5
Gabriel Gaia <gaiagabriel22@gmail.com>
2025-02-23 11:30:13 -0300

    feat: creates main function to validate CPF

--- src/cfg_cpf_validator/__init__.py
+++ src/cfg_cpf_validator/__init__.py
@@ -1,2 +1,24 @@
-def hello() -> str:
-    return "Hello from cfg-cpf-validator!"
+import typing as t
+
+import cfg_cpf_validator.cfg as c
+
+
+def validate(
+    cpf: t.Union[
+        str,
+        int,
+    ],
+) -> bool:
+    """
+    Valida um CPF.
+
+    Parâmetros:
+        cpf (str | int): CPF a ser validado.
+
+    Retorna:
+        bool: Indica se o CPF é válido.
+    """
+
+    structure = c.validate_structure(cpf)
+
+    return structure
