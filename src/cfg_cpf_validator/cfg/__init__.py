a8b0fd710b1a296c3392c34bd500bf078ef7b86a
franc <francadourado@gmail.com>
2025-02-23 16:16:26 -0300

    Adicionado calculo de digito verificador

--- src/cfg_cpf_validator/cfg/__init__.py
+++ src/cfg_cpf_validator/cfg/__init__.py
@@ -76,3 +76,35 @@ def validate_check_digits(
         return bool(tree)
     except ValueError:
         return False
+
+def calcular_digito_verificador(cpf_base) -> list:
+    """
+    Calcula dígitos verificadores do CPF fornecido
+
+    Parâmetros:
+        cpf_base (str): CPF a ser calculado
+
+    Retorna:
+        list: Lista com os dígitos verificadores
+    """
+    # Remover carácteres não numericos do cpf
+    cpf_base = ''.join(filter(str.isdigit, cpf_base))
+
+    # Função para cálculo padrão de digitos
+    def calc_digit(cpf: str, multiplicadores: list, ) -> int:
+        soma = 0
+        for i in range(len(multiplicadores)):
+            soma += int(cpf[i]) * multiplicadores[i]
+            resto = soma % 11
+        return 0 if resto < 2 else 11 - resto
+
+    # Cálculo do 1° digito
+    multiplicadores = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    d1 = calc_digit(cpf_base, multiplicadores)
+
+    # Cálculo do 2° digito
+    multiplicadores.insert(0, 11)
+    d2 = calc_digit(cpf_base, multiplicadores)
+
+    return [d1, d2]
+
