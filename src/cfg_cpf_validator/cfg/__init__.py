e03e7a766fc22b8a12860679c83caa27a9232c55
Gabriel Gaia <gaiagabriel22@gmail.com>
2025-02-23 11:07:32 -0300

    feat: adds method to validate CPF structure

---
+++ src/cfg_cpf_validator/cfg/__init__.py
@@ -0,0 +1,34 @@
+import typing as t
+
+import nltk as n
+
+CFG_STRUCTURE = n.CFG.fromstring(
+    """
+    S -> D D D D D D D D D D D | D D D '.' D D D '.' D D D '-' D D
+    D -> '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    """
+)
+
+structure_parser = n.parse.ChartParser(CFG_STRUCTURE)
+
+
+def cfg_validate_structure(
+    cpf: t.Union[
+        str,
+        int,
+    ],
+) -> bool:
+    """
+    Valida estrutura de um CPF com base em uma Gramática Livre de Contexto.
+
+    Parâmetros:
+        cpf (str | int): CPF a ser validado.
+
+    Retorna:
+        bool: Indica se a estrutura do CPF é válida.
+    """
+    tokens = list(str(cpf))
+    tree = list()
+    for leaf in structure_parser.parse(tokens):
+        tree.append(leaf)
+    return bool(tree)
